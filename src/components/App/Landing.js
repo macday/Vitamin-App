99555bae68d901ed41537da99aa1b5738bccddb4
Nahzilian <50055124+Nahzilian@users.noreply.github.com>
2020-02-04 22:50:34 -0500

    Added form

--- src/components/App/Landing.js
+++ src/components/App/Landing.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { Fragment } from 'react';
 import Button from './Button'
 import Navbar from '../header/Navbar'
 import './../styles/Landing.css'
+import { Route } from 'react-router'
 
+//Need to install React-router-dom react router --> easier control
 export default function App() {
     return (
-        <div className="Landing">
-            <Navbar />
-            <div className='container'>
-                <div className='top'>
-                    <Button id='Sign up' />
-                    <Button id='Login' />
-                </div>
-                <div className='about-content'>
+        <Fragment>
+            <div className="Landing">
+                <Navbar />
+                <div className='container'>
+                    <div className='top'>
+                        <Button id='Sign up' />
+                        <Button id='Login' />
+                    </div>
+                    <div className='about-content'>
 
+                    </div>
                 </div>
             </div>
-        </div>
+        </Fragment>
     )
 }
--- src/components/Form/Login.js
+++ src/components/Form/Login.js
@@ -4,9 +4,21 @@ import Button from '../App/Button';
 export class Login extends Component {
     render() {
         return (
-            <div>
-                <h1>This is the Login form</h1>
+            <div className = "container login-form">
+                <form>
+                    <div className="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    </div>
+                    <Button id ="Log in" />
+                    <a href = "">Doesn't have an account? <span className = "link-signup">Sign up here</span></a>
+                </form>
             </div>
+
         )
     }
 }
