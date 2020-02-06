1d81d9625f8383f170bccd44d011099cd4245cdc
macday <mckenziewjday@gmail.com>
2020-02-06 13:26:58 -0500

    Updated button functionality

--- src/components/App/Button.js
+++ src/components/App/Button.js
@@ -4,7 +4,7 @@ import './../styles/Button.css'
 export default function Button(props) {
     return (
         <div className='Button mx-3 my-1 rounded-pill'>
-            <button type='button' className='btn btn-lg text-nowrap px-5 mx-3'>
+            <button type='button' className='btn btn-lg text-nowrap px-5 mx-3' onClick={props.handleClick(props.id)}>
                 {props.id}
             </button>
         </div>
--- src/components/App/Landing.js
+++ src/components/App/Landing.js
@@ -1,33 +1,48 @@
 import React from 'react'
 import Button from './Button'
 import Navbar from '../header/Navbar'
+import Signup from './Signup'
 import './../styles/Landing.css'
 
-export default function App() {
-    return (
-        <div className="Landing">
-            <div className='navbar sticky-top bg-dark d-flex flex-row flex-nowrap justify-content-between'>
-                <div className='nav-item d-flex flex-row justify-content-start align-content-center'>
-                    <Navbar />
+export default class Landing extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    handleClick(elemID) {
+        console.log({ elemID }, ' clicked')
+        // ReactDOM.render(< { elemID } />, document.getElementById('root'));
+    }
+
+    render() {
+        return (
+            <div className="Landing">
+                <div className='navbar sticky-top bg-dark d-flex flex-row flex-nowrap justify-content-between'>
+                    <div className='nav-item d-flex flex-row justify-content-start align-content-center'>
+                        <Navbar />
+                    </div>
+                    <div class="nav-item d-flex flex-row justify-content-end align-content-center py-1">
+                        <Button id='Sign up' onClick={this.handleClick()} />
+                        <Button id='Login' />
+                    </div>
                 </div>
-                <div class="nav-item d-flex flex-row justify-content-end align-content-center py-1">
-                    <Button id='Sign up' />
-                    <Button id='Login' />
+                <div className='container-fluid about-content p-5'>
+                    <div className='row d-flex flex-row flex-wrap'>
+                        <div className='col border border-light'>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, quo consectetur provident molestiae soluta nisi sit illo id, eius nesciunt inventore. Ullam voluptatem nostrum culpa eius neque facilis, dicta esse officiis pariatur asperiores distinctio deserunt modi recusandae eligendi. Quasi temporibus molestias natus rem consectetur eius nobis, dolorem nemo ad incidunt cum veritatis odio reprehenderit illum. Suscipit temporibus mollitia libero ea praesentium minus quam doloremque possimus est ipsa fugiat laboriosam nesciunt explicabo nihil ducimus corporis, doloribus eum voluptates sapiente neque sunt at soluta adipisci? Modi qui possimus pariatur dignissimos animi quos ipsam explicabo sint ut? Corrupti cumque aperiam ad culpa eveniet.
+                    </div>
+                        <div className='col border border-dark'>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, quo consectetur provident molestiae soluta nisi sit illo id, eius nesciunt inventore. Ullam voluptatem nostrum culpa eius neque facilis, dicta esse officiis pariatur asperiores distinctio deserunt modi recusandae eligendi. Quasi temporibus molestias natus rem consectetur eius nobis, dolorem nemo ad incidunt cum veritatis odio reprehenderit illum. Suscipit temporibus mollitia libero ea praesentium minus quam doloremque possimus est ipsa fugiat laboriosam nesciunt explicabo nihil ducimus corporis, doloribus eum voluptates sapiente neque sunt at soluta adipisci? Modi qui possimus pariatur dignissimos animi quos ipsam explicabo sint ut? Corrupti cumque aperiam ad culpa eveniet.
+                    </div>
+                        <div className='col border border-light'>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, quo consectetur provident molestiae soluta nisi sit illo id, eius nesciunt inventore. Ullam voluptatem nostrum culpa eius neque facilis, dicta esse officiis pariatur asperiores distinctio deserunt modi recusandae eligendi. Quasi temporibus molestias natus rem consectetur eius nobis, dolorem nemo ad incidunt cum veritatis odio reprehenderit illum. Suscipit temporibus mollitia libero ea praesentium minus quam doloremque possimus est ipsa fugiat laboriosam nesciunt explicabo nihil ducimus corporis, doloribus eum voluptates sapiente neque sunt at soluta adipisci? Modi qui possimus pariatur dignissimos animi quos ipsam explicabo sint ut? Corrupti cumque aperiam ad culpa eveniet.
+                    </div>
+                    </div>
                 </div>
             </div>
-            <div className='container-fluid about-content p-5'>
-                <div className='row d-flex flex-row flex-wrap'>
-                    <div className='col border border-light'>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, quo consectetur provident molestiae soluta nisi sit illo id, eius nesciunt inventore. Ullam voluptatem nostrum culpa eius neque facilis, dicta esse officiis pariatur asperiores distinctio deserunt modi recusandae eligendi. Quasi temporibus molestias natus rem consectetur eius nobis, dolorem nemo ad incidunt cum veritatis odio reprehenderit illum. Suscipit temporibus mollitia libero ea praesentium minus quam doloremque possimus est ipsa fugiat laboriosam nesciunt explicabo nihil ducimus corporis, doloribus eum voluptates sapiente neque sunt at soluta adipisci? Modi qui possimus pariatur dignissimos animi quos ipsam explicabo sint ut? Corrupti cumque aperiam ad culpa eveniet.
-                    </div>
-                    <div className='col border border-dark'>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, quo consectetur provident molestiae soluta nisi sit illo id, eius nesciunt inventore. Ullam voluptatem nostrum culpa eius neque facilis, dicta esse officiis pariatur asperiores distinctio deserunt modi recusandae eligendi. Quasi temporibus molestias natus rem consectetur eius nobis, dolorem nemo ad incidunt cum veritatis odio reprehenderit illum. Suscipit temporibus mollitia libero ea praesentium minus quam doloremque possimus est ipsa fugiat laboriosam nesciunt explicabo nihil ducimus corporis, doloribus eum voluptates sapiente neque sunt at soluta adipisci? Modi qui possimus pariatur dignissimos animi quos ipsam explicabo sint ut? Corrupti cumque aperiam ad culpa eveniet.
-                    </div>
-                    <div className='col border border-light'>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, quo consectetur provident molestiae soluta nisi sit illo id, eius nesciunt inventore. Ullam voluptatem nostrum culpa eius neque facilis, dicta esse officiis pariatur asperiores distinctio deserunt modi recusandae eligendi. Quasi temporibus molestias natus rem consectetur eius nobis, dolorem nemo ad incidunt cum veritatis odio reprehenderit illum. Suscipit temporibus mollitia libero ea praesentium minus quam doloremque possimus est ipsa fugiat laboriosam nesciunt explicabo nihil ducimus corporis, doloribus eum voluptates sapiente neque sunt at soluta adipisci? Modi qui possimus pariatur dignissimos animi quos ipsam explicabo sint ut? Corrupti cumque aperiam ad culpa eveniet.
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+        )
+    }
 }
